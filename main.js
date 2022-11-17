d920fe1d73526d2a9a347bcc7c44761cbfbd8a58
eddieMakers87 <111758081+eddieMakers87@users.noreply.github.com>
2022-11-17 20:51:56 +0600

    crud

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+let API = "http://localhost:8000/posts";
+let API_USERS = "http://localhost:8000/profile";
+
+//! регистрация
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+let submit = document.querySelector("#submit");
+let first = document.querySelector(".remFirst");
+let second = document.querySelector(".remSecond");
+
+submit.addEventListener("click", async () => {
+  let obj = {
+    username: username.value,
+    password: password.value,
+  };
+
+  if (!obj.username.trim() || !obj.password.trim()) {
+    alert("fill the form field");
+    return;
+  }
+  console.log(obj);
+  await fetch(API_USERS, {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then(() => alert("Пользователь добавлен"));
+});
+
 // ! CRUD
 let inp = document.querySelector(".inp");
 let image = document.querySelector("#image");
